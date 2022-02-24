1c66135f131dccab91f219dd85a91c40dd84a01d
Dũng Tiến <trantiendung740@gmail.com>
2022-02-24 11:25:11 +0700

    edit  migriation

--- database/migrations/2022_02_23_140750_create_customers_table.php
+++ database/migrations/2022_02_23_140750_create_customers_table.php
@@ -19,7 +19,7 @@ class CreateCustomersTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->int('phone');
+            $table->string('phone');
             $table->string('address');
             $table->date('birhday');
             $table->integer('gender');
